910a12fd6caa4d773a6a72ad59a083c03724b365
Gülcin Civelek <gulcinncvlk@gmail.com>
2025-12-17 17:09:08 +0300

    Feat:Ödeme işlemleri için payment sınıfı eklendi.

---
+++ VehicleRentalSystemmmmm/src/main/java/org/example/Payment.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Payment {
+    // Odeme islemleri burada yapilacak
+    public void processPayment(double amount) {
+        System.out.println("Odeme alindi: " + amount + " TL");
+    }
+}
