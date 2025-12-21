47c48aa749800adf658b24d95ea2b5b1c0baa515
Gülcin Civelek <gulcinncvlk@gmail.com>
2025-12-21 09:08:14 +0300

    Ödeme işlemleri için kart doğrulama(valideteCard)simülasyonu eklendi.

--- VehicleRentalSystemmmmm/src/main/java/org/example/Payment.java
+++ VehicleRentalSystemmmmm/src/main/java/org/example/Payment.java
@@ -5,4 +5,15 @@ public class Payment {
     public void processPayment(double amount) {
         System.out.println("Odeme alindi: " + amount + " TL");
     }
+    // Kredi karti numarasinin gecerliligini kontrol eden metot
+    public boolean validateCard(String cardNumber) {
+        // Kart numarasi 16 haneli olmalidir (Basit kural)
+        if (cardNumber != null && cardNumber.length() == 16) {
+            System.out.println("Kart bilgileri dogrulandi.");
+            return true;
+        } else {
+            System.out.println("Hata: Gecersiz kart numarasi! Lutfen kontrol edin.");
+            return false;
+        }
+    }
 }
